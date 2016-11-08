bf7ab1f20d35bb097a19e902c764aaee75a830f4
Miguel Boland <mb1069@york.ac.uk>
2016-11-08 15:04:13 -0500

    Cleaned up part 1.1

--- part1/stream.py
+++ part1/stream.py
@@ -1,7 +1,5 @@
-import operator
-
 class Lfsr:
-
+    # Initialisation method for a LFSR
     def __init__(self, length, taps, initial_val):
         self.taps = []
         self.length = length
@@ -9,70 +7,28 @@ class Lfsr:
             self.taps.append(tap)
         self.reg_val = initial_val
 
+    # Calculates the tap value for the current LFSR's state
     def calc_tap(self):
         val = self.reg_val[self.taps[0]]
         for x in self.taps[1::]:
             val = (val ^ self.reg_val[x])
         return val
 
+    # Calculate the tap value, shift the register and return the
+    # value outputted by the register
     def shift(self):
         new_val = self.calc_tap()
         self.reg_val = [new_val] + self.reg_val
         return self.reg_val.pop()
 
-    def set_reg(self, new_reg_val):
-        self.reg_val = new_reg_val
-
-    def get_reg(self):
-        return self.reg_val
-    def get_taps(self):
-        return self.taps
-
-    def get_length(self):
-        return self.length
-
-def test_individual(lfsr, num_vals):
-    out = ""
-    for x in range(num_vals):
-        out += str(lfsr.shift())
-    return out
-
+# Implements the boolean function combining LFSR outputs
 def combine_lfsr_outputs(out_1, out_2, out_3):
-    if out_1==0 and out_2==0 and out_3==0:
-        return "1"
-    elif out_1==0 and out_2==0 and out_3==1:
-        return "1"
-    elif out_1==0 and out_2==1 and out_3==0:
-        return "0"
-    elif out_1==0 and out_2==1 and out_3==1:
-        return "1"
-    elif out_1==1 and out_2==0 and out_3==0:
-        return "0"
-    elif out_1==1 and out_2==0 and out_3==1:
-        return "0"
-    elif out_1==1 and out_2==1 and out_3==0:
-        return "1"
-    elif out_1==1 and out_2==1 and out_3==1:
-        return "0"
+    val = str(out_1) + str(out_2) + str(out_3)
+    val = int(val, 2)
+    sub_arr = [1, 1, 0, 1, 0, 0, 1, 0]
+    return str(sub_arr[val])
 
-
-def test_combined(lfsr1, lfsr2, lfsr3, num_vals):
-    out = ""
-    for x in range(num_vals):
-        out_1 = lfsr1.shift()
-        out_2 = lfsr2.shift()
-        out_3 = lfsr3.shift()
-        out += combine_lfsr_outputs(out_1, out_2, out_3)
-    print out
-
-def calc_agreement(seq1, seq2):
-    length = len(seq1)
-    agreements = 0
-    for x in range (0, length):
-        if (seq1[x]==seq2[x]):
-            agreements += 1
-    return float(agreements)/float(length)
-
+# Static instantiation of every LFSR
 arr0 = [1,1,0,0,0,0,1]
 l0 = Lfsr(7, [5, 6], arr0)
 
@@ -82,18 +38,16 @@ l1 = Lfsr(11, [8, 10], arr1)
 arr2 = [1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0]
 l2 = Lfsr(13, [7, 10, 11, 12], arr2)
 
+
 def main():
-    num_vals = 25
-
-    print "L0 output: "
-    print test_individual(l0, num_vals)
-    print "L2 output: "
-    print test_individual(l1, num_vals)
-    print "L3 output: "
-    print test_individual(l2, num_vals)
-
-    print "\nChallenge output: "
-    test_combined(l0, l1, l2, num_vals)
+    # Shift and combine output of LFSR's for 25 bits
+    out = ""
+    for x in range(25):
+        out_0 = l0.shift()
+        out_1 = l1.shift()
+        out_2 = l2.shift()
+        out += combine_lfsr_outputs(out_0, out_1, out_2)
+    print "\nChallenge output: " + str(out)
 
 
 if __name__ == "__main__":

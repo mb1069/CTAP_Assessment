069181aebb1f2a9a52bd49ff74497d5a496361f1
Miguel Boland <miguelboland@hotmail.com>
2016-11-16 17:29:00 +0000

    Added comments to block.py, added further details to report

--- part2/block.py
+++ part2/block.py
@@ -1,4 +1,4 @@
-
+# Convert a decimal number into a chunked 16 bit binary number, outputted in an array of chunks of 4 bits
 def dec2bin_chunks(key_val):
     val = "{0:b}".format(key_val)
     while len(val)<16:
@@ -6,24 +6,24 @@ def dec2bin_chunks(key_val):
     n = 4
     return [val[i:i+n] for i in range(0, len(val), n)]
 
+# Convert a decimal to a binary string of minimum length
 def dec2bin(val, length):
     out = "{0:0b}".format(val)
     while len(out)<length:
         out = "0"+out
     return out
 
+# Convert a binary value to a decimal
 def bin2dec(val):
     return int(val, 2)
 
-
+# Convert
 def substitute(in_val):
     arr = [0x4, 0x0, 0xC, 0x3, 0x8, 0xB, 0xA, 0x9, 0xD, 0xE, 0x2, 0x7, 0x6, 0x5, 0xF, 0x1]
     return arr[int(in_val)]
 
 def do_substitution(val):
     bin_vals = dec2bin_chunks(val)
-    # print dec2bin(val, 16)
-    # print bin_vals
     sub_vals = [dec2bin(substitute(bin2dec(x)),4) for x in bin_vals]
     sub_vals = "".join(sub_vals)
     return bin2dec(sub_vals)

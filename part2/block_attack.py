4df91b7e0731964b5a271a1657101991468f9f57
Miguel Boland <miguelboland@hotmail.com>
2016-11-17 17:03:30 +0000

    Cracked subkey for 2.2

--- part2/block_attack.py
+++ part2/block_attack.py
@@ -1,4 +1,10 @@
 
+# Assessment
+s_box = [0x4, 0x0, 0xC, 0x3, 0x8, 0xB, 0xA, 0x9, 0xD, 0xE, 0x2, 0x7, 0x6, 0x5, 0xF, 0x1]
+# Practice
+# s_box = [0xE, 0x4, 0xD, 0x1, 0x2, 0xF, 0xB, 0x8, 0x3, 0xA, 0x6, 0xC, 0x5, 0x9, 0x0, 0x7]
+
+
 def dec2bin_chunks(key_val):
     val = "{0:b}".format(key_val)
     while len(val)<16:
@@ -17,15 +23,22 @@ def bin2dec(val):
 
 
 def substitute(in_val):
-    # arr = [0x4, 0x0, 0xC, 0x3, 0x8, 0xB, 0xA, 0x9, 0xD, 0xE, 0x2, 0x7, 0x6, 0x5, 0xF, 0x1]
-    arr = [0xE, 0x4, 0xD, 0x1, 0x2, 0xF, 0xB, 0x8, 0x3, 0xA, 0x6, 0xC, 0x5, 0x9, 0x0, 0x7]
-    return arr[int(in_val)]
+    return s_box[int(in_val)]
 
 def do_substitution(val):
     bin_vals = dec2bin_chunks(val)
     # print dec2bin(val, 16)
     # print bin_vals
     sub_vals = [dec2bin(substitute(bin2dec(x)),4) for x in bin_vals]
+    sub_vals = "".join(sub_vals)
+    return bin2dec(sub_vals)
+
+def reverse_substitute(in_val):
+    return s_box.index(int(in_val))
+
+def do_reverse_substitution(val):
+    bin_vals = dec2bin_chunks(val)
+    sub_vals = [dec2bin(reverse_substitute(bin2dec(x)),4) for x in bin_vals]
     sub_vals = "".join(sub_vals)
     return bin2dec(sub_vals)
 
@@ -37,21 +50,76 @@ def permute(val):
 def apply_subkey(val, subkey):
     return val ^ subkey
 
-def round(val, subkey, last):
-    val = apply_subkey(val, subkey)
+def round(val, last):
     val = do_substitution(val)
     if not last:
         val = permute(val)
     return val
 
+def calculate_xor_profiles():
+    inputs = 15
+
+    maxs = 0
+    differential_pairs = []
+    for x in range(0, 15):
+        for y in range(0, 15):
+            s = 0
+            for z in range(0, 15):
+                if (s_box[z] ^ s_box[z ^ x]) == y:
+                    s +=1
+            if not (x==0 and y==0):
+                if (maxs < s):
+                    maxs = s
+            differential_pairs.append((x,y,s))
+    # sorted(differential_pairs, key=lambda x: x[2])
+    differential_pairs.sort(key = lambda x:x[2], reverse=True)
+    print differential_pairs
+
+def check_wtf():
+    vals = {}
+    subkeys = []
+    diff = 17
+    cipher_diff = 1
+    mask = 0xF
+    shift = 0
+    prev_key = 0xF
+    with open('block.txt', 'r') as myfile:
+        for line in myfile:
+            (key, val) = line.split()
+            vals[int(key)] = int(val)
+        for subkey in range(0,15):
+            prob = 0
+            for plaintext in vals:
+                cipher1 = vals[plaintext]
+                cipher2 = vals[plaintext^diff]
+                cipher1 = (cipher1 ^ ((subkey<<shift) & prev_key)) & mask
+                cipher2 = (cipher2 ^ ((subkey<<shift) & prev_key)) & mask
+
+                cipher1 = do_reverse_substitution(cipher1)
+                cipher2 = do_reverse_substitution(cipher2)
+                if (cipher1^cipher2)==(cipher_diff & mask):
+                    prob = prob + 1
+            prob = float(prob) / len(vals)
+            subkeys.append((subkey, prob))
+            print subkey, prob, prob*len(vals), len(vals)
+    subkeys.sort(key = lambda x:x[1], reverse=True)
+    print subkeys
 
 
-def do_4_rounds(val, subkeys):
+
+    # for every subkey
+        # for every line in block.txt
+        # XOR with subkey
+        # reverse substitute
+        # check difference with difference in plaintext at that stage
+
+
+
+def do_3_rounds(val):
     for x in range(0, 4):
-        print " "
-        val = round(val, subkeys[x], x>2)
-        print "Round: {0} Val: {2} Subkey: {1} Last: {3}".format(x, subkeys[x], val, x==4)
-    val = apply_subkey(val, subkeys[4])
+        # print " "
+        val = round(val, x>2)
+        # print "Round: {0} Val: {1} Last: {2}".format(x, val, x==4)
     return val
 
 def main():
@@ -66,8 +134,8 @@ def main():
     # print permute(15)
     # print permute(23130)
     subkeys = [4132,  8165,  14287,  54321, 53124]
-    print do_4_rounds(13571, subkeys)
-    for i in range(15):
+    check_wtf()
+    # calculate_xor_profiles()
 
 
 if __name__=="__main__":
